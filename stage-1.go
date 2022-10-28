0d5acba851f3d029f4febe05652ca7ecc7176869
adeben33 <adeniyiebenezer33@gmail.com>
2022-10-28 13:41:05 +0100

    addition of cors library

--- stage-1.go
+++ stage-1.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -12,9 +11,25 @@ type Info struct {
 	Bio           string `json:"bio"`
 }
 
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
 func main() {
 	router := gin.Default()
-	router.Use(cors.Default())
+	//router.Use(cors.Default())
+	router.Use(CORSMiddleware())
 	router.GET("/", getting)
 	router.Run(":8080")
 }
