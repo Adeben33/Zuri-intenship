b75c6e50b2738532f01b68ecd3ae6cc5dc7a8da9
adeben33 <adeniyiebenezer33@gmail.com>
2022-10-28 10:34:47 +0100

    This is the stage one assginment

---
+++ stage-1.go
@@ -0,0 +1,26 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+type Info struct {
+	SlackUsername string `json:"slack_username"`
+	Backend       bool   `json:"backend"`
+	Age           int    `json:"age"`
+	Bio           string `json:"bio"`
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/", getting)
+	router.Run(":8080")
+}
+
+func getting(ctx *gin.Context) {
+	details := Info{
+		SlackUsername: "Adeben33",
+		Backend:       true,
+		Age:           25,
+		Bio:           "I am a beginner golang developer interest in creating worthy portfolio project",
+	}
+	ctx.JSON(200, details)
+}
