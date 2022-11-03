8bc78cc5511481c3bf4ae8247025aad6f7edc04a
adeben33 <adeniyiebenezer33@gmail.com>
2022-11-03 22:42:59 +0100

    Changes in the operation

--- stage-1.go
+++ stage-1.go
@@ -12,7 +12,6 @@ type Info struct {
 	Age           int    `json:"age"`
 	Bio           string `json:"bio"`
 }
-
 type Operation struct {
 	OperationType string `json:"operation_type"`
 	X             int    `json:"x"`
@@ -25,12 +24,10 @@ func CORSMiddleware() gin.HandlerFunc {
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
-
 		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(204)
 			return
 		}
-
 		c.Next()
 	}
 }
@@ -42,49 +39,55 @@ func main() {
 	router.POST("/operation", enum)
 	router.Run(":8080")
 }
-
 func enum(c *gin.Context) {
 	var newOperation Operation
 	if err := c.BindJSON(&newOperation); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-
 	type returnStruct struct {
 		SlackUsername string `json:"slackUsername"`
 		Result        int    `json:"result"`
 		OperationType string `json:"operation_Type"`
 	}
 
-	if strings.Index(newOperation.OperationType, "add") != -1 {
-		var addedNumberReturnStruct returnStruct
-		addedNumber := newOperation.X + newOperation.Y
-		addedNumberReturnStruct.SlackUsername = "Adeben33"
-		addedNumberReturnStruct.Result = addedNumber
-		addedNumberReturnStruct.OperationType = "Addition"
-		c.JSON(200, addedNumberReturnStruct)
-	} else if strings.Index(newOperation.OperationType, "sub") != -1 {
-		var NumberReturnStruct returnStruct
-		Number := newOperation.X - newOperation.Y
-		NumberReturnStruct.SlackUsername = "Adeben33"
-		NumberReturnStruct.Result = Number
-		NumberReturnStruct.OperationType = "Subtraction"
-		c.JSON(200, NumberReturnStruct)
-	} else if strings.Index(newOperation.OperationType, "mul") != -1 {
-		var NumberReturnStruct returnStruct
-		Number := newOperation.X * newOperation.Y
-		NumberReturnStruct.SlackUsername = "Adeben33"
-		NumberReturnStruct.Result = Number
-		NumberReturnStruct.OperationType = "Multiplication"
-		c.JSON(200, NumberReturnStruct)
+	if newOperation.OperationType == "addition" {
+		if strings.Index(newOperation.OperationType, "add") != -1 {
+			var addedNumberReturnStruct returnStruct
+			addedNumber := newOperation.X + newOperation.Y
+			addedNumberReturnStruct.SlackUsername = "Adeben33"
+			addedNumberReturnStruct.Result = addedNumber
+			addedNumberReturnStruct.OperationType = newOperation.OperationType
+			addedNumberReturnStruct.OperationType = "addition"
+			c.JSON(200, addedNumberReturnStruct)
+		} else if newOperation.OperationType == "subtraction" {
+		} else if strings.Index(newOperation.OperationType, "sub") != -1 {
+			var NumberReturnStruct returnStruct
+			Number := newOperation.X - newOperation.Y
+			NumberReturnStruct.SlackUsername = "Adeben33"
+			NumberReturnStruct.Result = Number
+			NumberReturnStruct.OperationType = newOperation.OperationType
+			NumberReturnStruct.OperationType = "subtraction"
+			c.JSON(200, NumberReturnStruct)
+		} else if newOperation.OperationType == "multiplication" {
+		} else if strings.Index(newOperation.OperationType, "mul") != -1 {
+			var NumberReturnStruct returnStruct
+			Number := newOperation.X * newOperation.Y
+			NumberReturnStruct.SlackUsername = "Adeben33"
+			NumberReturnStruct.Result = Number
+			NumberReturnStruct.OperationType = newOperation.OperationType
+			NumberReturnStruct.OperationType = "multiplication"
+			c.JSON(200, NumberReturnStruct)
+		}
 	}
 }
-func getting(ctx *gin.Context) {
+
+func getting(c *gin.Context) {
 	details := Info{
 		SlackUsername: "Adeben33",
 		Backend:       true,
 		Age:           25,
 		Bio:           "I am a beginner golang developer interest in creating worthy portfolio project",
 	}
-	ctx.PureJSON(200, details)
+	c.PureJSON(200, details)
 }
